d3008ceeb0b291be51d19b6ec03d0b5bbb7abd8a
Your Name <your.email@example.com>
2025-02-28 11:53:44 -0600

    Create templates and views for Topics & Topic entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,8 +4,12 @@ from django.urls import path
 
 from .import views
 
-app_name = 'tech_journal'
+app_name = 'tech_journal' # This line allows Django to distinguish this urls.py file from other urls.py files in the project.
 urlpatterns = [
     # Home page.
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), # The name 'index' allows us to refer to this URL pattern when building links in templates.
+    # Show all topics.
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """The home page for Tech Journal."""
     return render(request, 'tech_journal/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'tech_journal/topics.html', context)
+
+def topic(request, topic_id): # topic_id is a parameter that matches the value captured by the regex in the URL pattern.
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id) # Retrieve the topic with the id captured by the URL pattern.
+    entries = topic.entry_set.order_by('-date_added') 
+    context = {'topic': topic, 'entries': entries} # Pass the topic and its associated entries to the template.
+    return render(request, 'tech_journal/topic.html', context)
